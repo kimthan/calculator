d8c6a571cfcd8b9a6aff58c1d68895bcf9effaa6
KT <kt@KTs-MacBook-Air.T-mobile.com>
2022-10-10 17:12:44 -0700

    all buttons done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
 function App() {
   return (
-    <div className="flex justify-center h-screen items-center">
-      <div className="w-[200px] h-[200px] border-2">Calculator</div>
+    <div className="flex justify-center h-screen items-center w-[500px] mx-auto ">
+      <div className="w-[200px] h-[300px]  grid gap-1 grid-cols-4 bg-black p-1 rounded-lg">
+        <div className="bg-gray-400 w-[full] h-[50px] col-span-4 m-3 rounded p-2 flex justify-end items-end">
+          result
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          AC
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          +/-
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          %
+        </div>
+        <div className="bg-orange-400 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          /
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          7
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          8
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          9
+        </div>
+        <div className="bg-orange-400 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          x
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          4
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          5
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          6
+        </div>
+        <div className="bg-orange-400 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          -
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          1
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          2
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          3
+        </div>
+        <div className="bg-orange-400 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          +
+        </div>
+        <div className="bg-gray-500 w-[80px] h-[35px] col-span-2 rounded-full mx-auto text-center flex items-center justify-center">
+          0
+        </div>
+        <div className="bg-gray-500 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          .
+        </div>
+        <div className="bg-orange-400 w-[35px] h-[35px] rounded-full mx-auto text-center flex items-center justify-center">
+          =
+        </div>
+      </div>
     </div>
   );
 }
